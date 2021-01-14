4046907902a4da09a67b2d23f6a7b3e80d63ccdc
fmartin92 <fdmartin92+github@gmail.com>
2021-01-14 20:39:31 -0300

    built site

---
+++ docs/build/precache-manifest.c47713df112c0b90d07eb73647db8ddb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99714d811c99ec98cb25f31f286ac713",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72f52d47893b7c3535fd",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "75f53e0fbe27c603291c",
+    "url": "/static/css/main.1fa6ef29.chunk.css"
+  },
+  {
+    "revision": "72f52d47893b7c3535fd",
+    "url": "/static/js/2.6d8a9435.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6d8a9435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f53e0fbe27c603291c",
+    "url": "/static/js/main.d4c0785f.chunk.js"
+  },
+  {
+    "revision": "bd23c8bfbda746277573",
+    "url": "/static/js/runtime-main.9bd828f2.js"
+  }
+]);
